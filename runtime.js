f577bafc3e0f4c8922d0ab6c26fab98c9927291e
zhoujunpeng <zjp0432@163.com>
2019-04-04 16:32:57 +0800

    add comments

--- runtime.js
+++ runtime.js
@@ -1,3 +1,11 @@
+/**
+ * @function initComponent 初始化组件
+ * @param {string} name 组件名称
+ * @param {Object} data 
+ * @param {string} data.template 组件模板结构
+ * @param {string} data.style 组件CSS代码
+ * @param {string} data.script 组件JavaScript代码
+ */
 function initComponent(name,data){
   const {template,style,script} = data;
   const $template = document.createElement('template');
